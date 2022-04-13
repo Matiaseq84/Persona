7542786751e041cc9d6c963918a2324cd0bd64bf
matiaseq <matiaseq@gmail.com>
2022-04-13 15:30:07 -0300

    CORS 1.2

--- src/main/java/com/portfolio/Persona/PersonaApplication.java
+++ src/main/java/com/portfolio/Persona/PersonaApplication.java
@@ -26,9 +26,9 @@ public class PersonaApplication {
 		corsConfiguration.setAllowedOrigins(Arrays.asList("https://portfoliomq.web.app/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-				"Access-Control-Request-Method", "Access-Control-Request-Headers", "Access-Control-Max-Age: 86400"));
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
 		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-				"Access-Control-Allow-Origin", "Access-Control-Allow_Origin", "Access-Control-Allow-Credentials", "Access-Control-Max-Age: 86400"));
+				"Access-Control-Allow-Origin", "Access-Control-Allow_Origin", "Access-Control-Allow-Credentials"));
 		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
