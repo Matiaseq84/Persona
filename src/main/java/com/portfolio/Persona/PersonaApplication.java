a040216f437d097022c9b6488a7a4f24d9268eea
matiaseq <matiaseq@gmail.com>
2022-04-13 19:14:20 -0300

    PAJEROKU

--- src/main/java/com/portfolio/Persona/PersonaApplication.java
+++ src/main/java/com/portfolio/Persona/PersonaApplication.java
@@ -23,12 +23,13 @@ public class PersonaApplication {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
 		//corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://portfoliomq.web.app/"));
+		//corsConfiguration.setAllowedOrigins(Arrays.asList("https://portfoliomq.web.app/"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+				"Access-Control-Request-Method", "Access-Control-Request-Headers", "x-auth-token"));
 		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-				"Access-Control-Allow-Origin", "Access-Control-Allow_Origin", "Access-Control-Allow-Credentials"));
+				"Access-Control-Allow-Origin", "Access-Control-Allow_Origin", "Access-Control-Allow-Credentials", "x-auth-token"));
 		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
