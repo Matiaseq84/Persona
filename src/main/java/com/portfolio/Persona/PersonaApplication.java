0fa86c94ba97865dca5327de74537f49dfe4a8c7
matiaseq <matiaseq@gmail.com>
2022-04-13 11:42:41 -0300

    HEROKU DEPLOY 1

--- src/main/java/com/portfolio/Persona/PersonaApplication.java
+++ src/main/java/com/portfolio/Persona/PersonaApplication.java
@@ -23,6 +23,7 @@ public class PersonaApplication {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
 		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		//corsConfiguration.setAllowedOrigins(Arrays.asList("https://portfoliomq.web.app/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
